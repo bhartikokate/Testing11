c8c09c47d41a737f7375dacc6572e6b93a67d176
Admin <Admin@DESKTOP-F9GJQDS>
2021-07-30 15:32:24 +0530

    by bharti

--- Git_Project/src/GIT_Demo/GITDemo1.java
+++ Git_Project/src/GIT_Demo/GITDemo1.java
@@ -3,5 +3,11 @@ package GIT_Demo;
 public class GITDemo1 {
 public static void main(String[] args) {
 	System.out.println("Git method");
+	
+	
+}
+public void m1()
+{
+	System.out.println("m1 mtd");
 }
 }
