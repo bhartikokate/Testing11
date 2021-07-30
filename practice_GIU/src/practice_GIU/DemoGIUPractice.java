4085ec3165e19e9e1f6f570582534fce991c7be9
Admin <Admin@DESKTOP-F9GJQDS>
2021-07-30 16:21:30 +0530

    by bharti

--- practice_GIU/src/practice_GIU/DemoGIUPractice.java
+++ practice_GIU/src/practice_GIU/DemoGIUPractice.java
@@ -4,7 +4,8 @@ public class DemoGIUPractice {
 	public void m1()
 	{
 		System.out.println("m1 mtd");
-		
+		System.out.println("This is DemoGIUPractice class ");
+		System.out.println("----------");
 	}
 
 	public static void main(String[] args) {
